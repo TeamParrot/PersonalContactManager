f6e91b5fd5f9218727d7d168903c170ef80b7628
bpglaser <bpglaser@gmail.com>
2019-01-25 13:28:14 -0500

    Updated contact to remove fields

--- backend/contact.py
+++ backend/contact.py
@@ -12,10 +12,6 @@ class Contact:
             self.lastname = raw['lastName']
             self.phonenumber = raw['phoneNumber']
             self.email = raw['email']
-            self.address = raw['address']
-            self.city = raw['city']
-            self.state = raw['state']
-            self.zip = raw['zip']
         except KeyError:
             raise MalformedContactError
 
@@ -25,8 +21,4 @@ class Contact:
             'lastName': self.lastname,
             'phoneNumber': self.phonenumber,
             'email': self.email,
-            'address': self.address,
-            'city': self.city,
-            'state': self.state,
-            'zip': self.zip
         }
