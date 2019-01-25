900d9ebf670dc88fe88b63e20a755ccd264eff64
bpglaser <bpglaser@gmail.com>
2019-01-25 13:45:06 -0500

    Impl temporary memory db

--- backend/database.py
+++ backend/database.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+import secrets
 import mysql.connector as connector
 
 
@@ -13,14 +15,14 @@ class UnauthorizedError(Exception):
     pass
 
 
+User = namedtuple('User', ['username', 'password', 'token', 'contacts'])
+
 class Database:
     def __init__(self, cfg):
-        self.conn = connector.connect(
-            host=cfg.db_host, database=cfg.db_name,
-            user=cfg.db_user, password=cfg.db_passwd)
+        self.users = []
 
     def close(self):
-        self.conn.close()
+        pass
 
     def login(self, username, password):
         """Authenticate the user and return a session token
@@ -28,11 +30,18 @@ class Database:
         Raises:
             UnauthorizedError: The credentials are incorrect.
         """
-        pass
+        for user in self.users:
+            if user.username == username and user.password:
+                user.token = secrets.token_hex()
+                return user.token
+        raise UnauthorizedError
 
     def logout(self, username):
         """Removes the user from the session table"""
-        pass
+        for user in self.users:
+            if user.username == username:
+                user.token = None
+                return
 
     def insert_user(self, username, password):
         """Creates a new user entry and logs them in.
@@ -43,7 +52,10 @@ class Database:
         Raises:
             ConflictError: The username is taken.
         """
-        pass
+        for user in self.users:
+            if user.username == username:
+                raise ConflictError
+        self.users.append(User(username, password, None, {}))
 
     def validate_login(self, token):
         """Checks if the given token is in the session table.
@@ -51,19 +63,37 @@ class Database:
         Raises:
             UnauthorizedError: The token isn't logged in.
         """
-        pass
+        assert token is not None
+
+        for user in self.users:
+            if user.token == token:
+                return user.username
+        raise UnauthorizedError
+
 
     def get_contacts(self, username):
         """Returns a list of Contacts associated with the given username."""
-        pass
+        for user in self.users:
+            if user.username == username:
+                return user.contacts.values()
+        return []
 
     def insert_contact(self, username, contact):
         """Creates a new contact and returns the contact_id."""
-        pass
+        for user in self.users:
+            if user.username == username:
+                key = secrets.token_hex()
+                user.contacts[key] = contact
+                return key
 
     def delete_contact(self, username, contact_id):
         """Deletes a contact if it exists."""
-        pass
+        for user in self.users:
+            if user.username == username:
+                if contact_id in user.contacts:
+                    del user.contacts[contact_id]
+                    return
+        raise ContactExistsError
 
     def update_contact(self, username, contact_id, contact):
         """Equivalent to deleting the old contact and creating a new one.
@@ -74,4 +104,5 @@ class Database:
         Raises:
             ContactExistsError: No entry with the given contact_id.
         """
-        pass
+        self.delete_contact(username, contact_id)
+        self.insert_contact(username, contact)
