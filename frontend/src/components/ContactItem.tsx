22890bb8d367108f0afe7ace39182f7954cb9164
Cyral <cyral@pyratron.com>
2019-01-26 18:58:21 -0500

    Dialog for adding and editing contacts

--- frontend/src/components/ContactItem.tsx
+++ frontend/src/components/ContactItem.tsx
@@ -3,13 +3,16 @@ import {IApiService} from "../services/IApiService";
 import App from "../App";
 import {Contact} from "../models/Contact";
 import "./ContactItem.scss";
+import {ContactDialog, ContactDialogMode} from "./ContactDialog";
 
 type ContactItemProps = {
     contact: Contact;
+    onDelete: () => void,
 };
 
 type ContactItemState = {
     contact: Contact;
+    showContactDialog?: boolean;
 };
 
 export class ContactItem extends Component<ContactItemProps, ContactItemState> {
@@ -19,35 +22,63 @@ export class ContactItem extends Component<ContactItemProps, ContactItemState> {
         super(props);
 
         this.state = {
-            contact: props.contact
+            contact: props.contact,
         };
     }
 
     render() {
-        return <div className="contact">
-            <div className="contact__info">
-                <div className="contact__name">
-                    {this.state.contact.firstName} {this.state.contact.lastName}
+        return <div>
+            <div className="contact">
+                <div className="contact__info">
+                    <div className="contact__name">
+                        {this.state.contact.firstName} {this.state.contact.lastName}
+                    </div>
+                    <div className="contact__email">
+                        {this.state.contact.email}
+                    </div>
+                    <div className="contact__phone-number">
+                        {this.state.contact.phoneNumber}
+                    </div>
                 </div>
-                <div className="contact__email">
-                    {this.state.contact.email}
-                </div>
-                <div className="contact__phone-number">
-                    {this.state.contact.phoneNumber}
+                <div className="contact__controls">
+                    <button onClick={this.editContact}>Edit</button>
+                    <button onClick={this.deleteContact}>Delete</button>
                 </div>
             </div>
-            <div className="contact__controls">
-                <button onClick={this.deleteContact}>Edit</button>
-                <button onClick={this.editContact}>Delete</button>
-            </div>
+            <ContactDialog showModal={this.state.showContactDialog}
+                           mode={ContactDialogMode.Edit}
+                           contact={this.state.contact}
+                           onCancel={this.contactDialogCancel}
+                           onSubmit={this.contactDialogSubmit}/>
         </div>
     }
 
-    private deleteContact = () => {
 
+    private contactDialogCancel = () => {
+        this.setState({
+            ...this.state,
+            showContactDialog: false,
+        });
+    };
+
+    private contactDialogSubmit = async (contact: Contact) => {
+        const editedContact = await this.api.updateContact(contact);
+        this.setState({
+            ...this.state,
+            contact: editedContact,
+            showContactDialog: false,
+        });
+    };
+
+    private deleteContact = async () => {
+        await this.api.deleteContact(this.state.contact.id);
+        this.props.onDelete();
     };
 
     private editContact = () => {
-
+        this.setState({
+            ...this.state,
+            showContactDialog: true,
+        });
     };
 }
--- frontend/src/services/MockApiService.ts
+++ frontend/src/services/MockApiService.ts
@@ -4,12 +4,13 @@ import {User} from "../models/User";
 
 export class MockApiService implements IApiService {
     public async login(username: string, password: string): Promise<User> {
-        throw new Error("Invalid password!");
-        //return new User("Demo Account")
+        //throw new Error("Invalid password!");
+        return new User("Demo Account")
     }
     
-    public async addContact(contact: Contact): Promise<void> {
-        return;
+    public async addContact(contact: Contact): Promise<Contact> {
+        contact.id = Math.floor(Math.random() * 100000);
+        return contact;
     }
 
     public async deleteContact(id: number): Promise<void> {
@@ -19,14 +20,14 @@ export class MockApiService implements IApiService {
     public async getContacts(): Promise<Contact[]> {
         return [
             {
-                "id": "1j4Fqp",
+                "id": 1,
                 "firstName": "bob",
                 "lastName": "dole",
                 "phoneNumber": "4075559999",
                 "email": "dummy@fakemail.net",
             },
             {
-                "id": "2rjp1Pq",
+                "id": 2,
                 "firstName": "jim",
                 "lastName": "henson",
                 "phoneNumber": "4075558888",
@@ -43,7 +44,7 @@ export class MockApiService implements IApiService {
         return;
     }
 
-    public async updateContact(contact: Contact): Promise<void> {
-        return;
+    public async updateContact(contact: Contact): Promise<Contact> {
+        return contact;
     }
 }
